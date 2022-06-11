0ac98e2fc43baf788956e21b44856b02e9787129
NatallyR <107313433+NatallyR@users.noreply.github.com>
2022-06-11 11:04:10 -0500

    Hola Mudo 1ra actualización

---
+++ curso logica/HolaMundo.py
@@ -0,0 +1,11 @@
+# EJEMPLO 1. SALUDO
+# COMENTARIOS: documentar, detallar cada parte del programa o código
+# los comentarios no los toma en cuenta Python y no se ejecutan
+
+# imprimir o mostrar resultados en pantalla de salida
+print("HOLA MUNDO")
+print('Natally Ramirez Neri');
+print(10+5)
+print(20-2)
+print(4*7)
+print(9/3)
